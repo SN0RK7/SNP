70a3ce73b822ce45205ce3377feb673437b05ad3
SN0RK7 <82673998+SN0RK7@users.noreply.github.com>
2022-01-20 20:53:54 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 const select = document.getElementById('direction');
+const tel = document.querySelector('#tel');
+const regex = /[a-zA-Zа-яА-Я]/g;
 
 select.addEventListener('change', function() {
   select.style.color = "#000"
 });
+
+tel.oninput = function(){
+  this.value = this.value.replace(regex, '');
+}
+
+// const header = document.querySelector("navbar");
+
+// window.addEventListener('scroll', function() {
+//   if (window.pageYOffset >= 720) {
+//     header.classList.add("sticky");
+//   } else {
+//     header.classList.remove("sticky");
+//   }
+// });
