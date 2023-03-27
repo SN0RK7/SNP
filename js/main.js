83cf71581258e287e8d4dfdec12c56518b9e9273
SN0RK7 <82673998+SN0RK7@users.noreply.github.com>
2023-03-27 19:49:44 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,17 +1,10 @@
 const direction = document.querySelector('#direction')
 const header = document.querySelector('#header');
-const resetBtn = document.querySelector('#reset_btn');
 const regex = /[A-Za-zA-Яа-яЁё]/g;
 const phone = document.querySelector('#phone')
 
-
 direction.addEventListener('change', function() {
   direction.style.color = "#000"
-})
-
-
-resetBtn.addEventListener('click', function () {
-    document.getElementById('direction').style.color = '#A6A6A6';
 })
 
 
@@ -22,7 +15,6 @@ window.addEventListener('scroll', function() {
     header.classList.remove('sticky');
   }
 })
-
 
 phone.oninput = function() {
   this.value = this.value.replace(regex, '');
